1fd8aca635abcd13122f4d5c6c72afbbcc8089fd
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-09-13 11:05:37 +0200

    getting priority and displaying badge

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,9 +1,10 @@
 import { NoteCtrl } from "./note";
 
-export const UICtrl = (function(){
+export const UICtrl = (function () {
   const selectors = {
     nav: 'nav',
     alert: '.alert',
+    noteEditor: '.note__editor',
     noteFormHeading: '#note-form h3',
     titleInput: '#title-input',
     priorityInput: '#priority-input',
@@ -16,92 +17,95 @@ export const UICtrl = (function(){
     deleteButton: '.delete-button',
     backButton: '.back-button'
   }
-  const getTodayDate = function(){
+  const getTodayDate = function () {
     const dateNow = new Date();
     let dateUTC = new Date(Date.UTC(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate()))
     return dateUTC.toISOString().slice(0, 10);
   }
   return {
-    getSelectors: function(){
+    getSelectors: function () {
       return selectors;
     },
-    getNotesInput: function(){
+    getNotesInput: function () {
+      const priorityInput = document.querySelector(selectors.priorityInput);
       return {
         title: document.querySelector(selectors.titleInput).value,
-        priority: document.querySelector(selectors.priorityInput).value,
+        priority: {
+          val: priorityInput.value,
+          text: priorityInput.selectedOptions[0].text
+        },
         date: document.querySelector(selectors.dateInput).value,
         body: document.querySelector(selectors.bodyInput).value
       }
     },
-    createNotesList: function(notes){
+    createNotesList: function (notes) {
       let html = '';
       notes.forEach(note => {
         html += `
-          <li class="list-item card" id="note-${note.id}">
-            <header>
-              <h3 class="list-item__heading">${note.title}</h3>
-              <span class="label">${note.priority}</span>
-              <span class="label date">${note.date}</span>
-            </header>
-            <p class="list-item__body">${note.body}</p>
-            <footer>
-              <a href="#" class="button success edit-note">edit</a>
-            </footer>
+          <li class="note list-item stack" id="note-${note.id}">
+            <div class="note__top-bar">
+              <span class="note__priority button ${note.priorityVal}">${note.priorityText}</span>
+              <span class="note__date date">${note.date}</span>
+              <a href="#" class="button edit-note">Edytuj</a>
+            </div>
+            <div class="note__inner">
+              <h3 class="note__title">${note.title}</h3>
+              <p class="block note__body">${note.body}</p>
+            </div>
           </li>
-        `   
+        `
       });
       const list = document.getElementById('notes-list');
       list.insertAdjacentHTML('afterbegin', html);
     },
-    addNoteListItem: function(note){
+    addNoteListItem: function (note) {
       const list = document.querySelector(selectors.notesList);
       const li = document.createElement('li');
-      li.className = 'list-item card';
+      li.className = 'note list-item stack';
       li.id = `note-${note.id}`;
       li.innerHTML = `
-        <header>
-          <h3 class="list-item__heading">${note.title}</h3>
-          <span class="label">${note.priority}</span>
-          <span class="label date">${note.date}</span>
-        </header>
-        <p class="list-item__body">${note.body}</p>
-        <footer>
-          <a href="#" class="button success edit-note">edit</a>
-        </footer>
+        <div class="note__top-bar">
+          <span class="note__priority button ${note.priorityVal}">${note.priorityText}</span>
+          <span class="note__date date">${note.date}</span>
+          <a href="#" class="button edit-note">Edytuj</a>
+        </div>
+        <div class="note__inner">
+          <h3 class="note__title">${note.title}</h3>
+          <p class="block note__body">${note.body}</p>
+        </div>
       `
       list.insertAdjacentElement('beforeend', li);
       document.querySelector(selectors.notesList).style.display = 'block';
     },
-    deleteNoteListItem: function(id){
+    deleteNoteListItem: function (id) {
       const noteId = `#note-${id}`;
       const noteToDelete = document.querySelector(noteId);
       noteToDelete.remove();
     },
-    updateNoteListItem: function(note){
+    updateNoteListItem: function (note) {
       let notesListItems = Array.from(document.querySelectorAll(selectors.notesListItem));
 
       notesListItems.forEach(listItem => {
         const listItemId = listItem.getAttribute('id');
 
-        if(listItemId === `note-${note.id}`){
-          console.log(listItem.getElementsByTagName('header'));
-          listItem.getElementsByTagName('header')[0].innerHTML = `
-            <h3 class="list-item__heading">${note.title}</h3>
-            <span class="label">${note.priority}</span>
-            <span class="label date">${note.date}</span>
+        if (listItemId === `note-${note.id}`) {
+          listItem.getElementsByClassName('note__top-bar')[0].innerHTML = `
+            <span class="note__priority">${note.priority}</span>
+            <span class="note__date date">${note.date}</span>
+            <a href="#" class="button success edit-note">edit</a>
           `
           listItem.getElementsByTagName('p')[0].innerHTML = note.body;
         }
       });
     },
-    showEditState: function(){
+    showEditState: function () {
       document.querySelector(selectors.noteFormHeading).textContent = 'Edycja notatki';
       UICtrl.showElement(selectors.updateButton, 'inline-block');
       UICtrl.showElement(selectors.deleteButton, 'inline-block');
       UICtrl.showElement(selectors.backButton, 'inline-block');
       UICtrl.hideElement(selectors.addButton);
     },
-    clearEditState: function(){
+    clearEditState: function () {
       document.querySelector(selectors.noteFormHeading).textContent = 'Dodaj notatkę';
       UICtrl.clearInputs();
       UICtrl.hideElement(selectors.updateButton);
@@ -109,7 +113,7 @@ export const UICtrl = (function(){
       UICtrl.hideElement(selectors.backButton);
       UICtrl.showElement(selectors.addButton, 'inline-block');
     },
-    setInputsToEdit(){
+    setInputsToEdit() {
       const note = NoteCtrl.getCurrentNote();
       document.querySelector(selectors.titleInput).value = note.title;
       document.querySelector(selectors.bodyInput).value = note.body;
@@ -118,32 +122,32 @@ export const UICtrl = (function(){
 
       UICtrl.showEditState();
     },
-    clearInputs: function(){
+    clearInputs: function () {
       document.querySelector(selectors.titleInput).value = '';
       document.querySelector(selectors.bodyInput).value = '';
       document.querySelector(selectors.priorityInput).options[0].selected = true;
       document.querySelector(selectors.dateInput).value = getTodayDate();
     },
-    showAlert(className, alertText){
-      const nav = document.querySelector(selectors.nav);
+    showAlert(className, alertText) {
+      const nav = document.querySelector(selectors.noteEditor);
       const alertDiv = document.createElement('div');
       alertDiv.className = (`alert label ${className}`);
       alertDiv.appendChild(document.createTextNode(alertText));
       nav.insertAdjacentElement('afterend', alertDiv);
-      setTimeout(()=>{
+      setTimeout(() => {
         UICtrl.clearAlert();
       }, 2500)
     },
-    clearAlert(){
+    clearAlert() {
       const alert = document.querySelector(selectors.alert);
-      if(alert){
+      if (alert) {
         alert.remove();
       }
     },
-    showElement: function(elem, displayStyle){
+    showElement: function (elem, displayStyle) {
       document.querySelector(elem).style.display = displayStyle;
     },
-    hideElement: function(elem){
+    hideElement: function (elem) {
       document.querySelector(elem).style.display = 'none';
     }
   }
